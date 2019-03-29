404201166f8e19a2071f5d32a8211d90642fa868
Kosta <31946393+kostaa-k@users.noreply.github.com>
2019-03-28 23:10:09 -0400

    Add files via upload

---
+++ Board.py
@@ -0,0 +1,188 @@
+'''
+Created on 2019 M03 26
+
+@author: kale3370
+'''
+from Pieces import piece
+
+class board:
+    
+    board_array = [[0 for x in range(8)] for y in range(8)] 
+    
+    
+    
+    def __init__(self):
+        
+        for x in range(0, 8):
+            for i in range(0,8):
+                temp = piece("", "", x, i)
+                self.board_array[x][i] = temp
+                
+                
+    
+    def set_board(self):
+        
+        #Setting white pawns
+        val = 1
+        for i in range(0, 8):
+            self.board_array[i][val].name = "pawn"
+            self.board_array[i][val].colour = "white" 
+        
+        #Setting black pawns
+        val = 6
+        for i in range(0, 8):
+            self.board_array[i][val].name = "pawn"
+            self.board_array[i][val].colour = "black"
+            
+        
+        #Setting white rooks:
+        self.board_array[0][0].name = "rook"
+        self.board_array[0][0].colour = "white"
+        
+        #Setting white rooks:
+        self.board_array[7][0].name = "rook"
+        self.board_array[7][0].colour = "white"
+        
+        #Setting black rooks:
+        self.board_array[0][7].name = "rook"
+        self.board_array[0][7].colour = "black"
+        
+        #Setting black rooks:
+        self.board_array[7][7].name = "rook"
+        self.board_array[7][7].colour = "black"
+        
+        #Setting white knights:
+        self.board_array[1][0].name = "knight"
+        self.board_array[1][0].colour = "white"
+        
+        #Setting white knights:
+        self.board_array[6][0].name = "knight"
+        self.board_array[6][0].colour = "white"
+        
+        #Setting black knights:
+        self.board_array[1][7].name = "knight"
+        self.board_array[1][7].colour = "black"
+        
+        #Setting white knights:
+        self.board_array[6][7].name = "knight"
+        self.board_array[6][7].colour = "black"
+        
+        
+        #Setting white bishops:
+        self.board_array[2][0].name = "bishop"
+        self.board_array[2][0].colour = "white"
+        
+        self.board_array[5][0].name = "bishop"
+        self.board_array[5][0].colour = "white"
+        
+        #Setting black bishops:
+        self.board_array[2][7].name = "bishop"
+        self.board_array[2][7].colour = "black"
+        
+        self.board_array[5][7].name = "bishop"
+        self.board_array[5][7].colour = "black"
+        
+        
+        
+        #Setting white queen:
+        self.board_array[3][0].name = "queen"
+        self.board_array[3][0].colour = "white"
+        
+        #Setting black queen
+        self.board_array[3][7].name = "queen"
+        self.board_array[3][7].colour = "black"
+        
+        
+        
+        #Setting white king:
+        self.board_array[4][0].name = "king"
+        self.board_array[4][0].colour = "white"
+        
+        #Setting black king:
+        self.board_array[4][7].name = "king"
+        self.board_array[4][7].colour = "black"
+        
+        
+        for x in range(0, 8):
+            for y in range(0, 8):
+                self.board_array[x][y].x = x
+                self.board_array[x][y].y = y
+        
+        
+        
+    def is_white_in_check(self):
+        
+        brd = self.board_array
+        
+        
+        for i in range(0, 8):
+            for j in range(0, 8):
+                temp_piece = brd[i][j]
+                if(temp_piece.colour == "black"):
+                    #print(temp_piece.colour+" - "+temp_piece.name)
+                    potentials = temp_piece.get_potential(self)
+                    
+                    if (potentials is not None):
+                        for x in potentials:
+                            if(x.name == "king" and x.colour == "white"):
+                                return True
+        
+        return False
+    
+    
+    def is_black_in_check(self):
+        
+        brd = self.board_array
+        
+        
+        for i in range(0, 8):
+            for j in range(0, 8):
+                temp_piece = brd[i][j]
+                if(temp_piece.colour == "white"):
+                    #print(temp_piece.colour+" - "+temp_piece.name)
+                    potentials = temp_piece.get_potential(self)
+                    
+                    if (potentials is not None):
+                        for x in potentials:
+                            if(x.name == "king" and x.colour == "black"):
+                                return True
+        
+        return False
+                    
+        
+    def print_board(self):
+        
+        for y in range(0,8):
+            for x in range(0,8):
+                temp_piece = self.board_array[x][y]
+                
+                if (temp_piece.name != ""):
+                    print(temp_piece.name[:1], " ",  end = "")
+                else:
+                    print("-  ", end ="")
+                
+            print()
+            
+            
+            
+    def get_pieces(self):
+        
+        return self.board_array
+            
+            
+            
+            
+def convert_spot(the_piece):
+    x = the_piece.x
+    y = the_piece.y+1
+    
+    alphas = ["a", "b", "c", "d", "e", "f", "g", "h"]
+    
+    letter = alphas[x]
+    
+    position_string = letter+(str)(y)
+    
+    return position_string
+
+
+    
---
+++ Pieces.py
@@ -0,0 +1,399 @@
+'''
+Created on 2019 M03 26
+
+@author: kale3370
+'''
+#from Board import board
+import Board
+#from Board import board
+
+class piece():
+    
+    name = ""
+    colour = ""
+    x = -1
+    y = -1
+    
+    def __init__(self, piece_name, the_colour, pos_x, pos_y):
+        
+        self.name = piece_name
+        self.colour = the_colour
+        self.x = pos_x
+        self.y = pos_y
+        
+        
+    def get_potential(self, board):
+        
+        potentials = []
+        
+        if (self.name == "pawn" and self.colour == "white"):
+            potentials = white_pawn_potential(self, board)
+        elif (self.name == "pawn" and self.colour == "black"):
+            potentials = black_pawn_potential(self, board)
+        elif(self.name == "rook"):
+            potentials1 = left_right_potential(self, board)
+            potentials2 = up_down_potential(self, board)
+            potentials = potentials1.extend(potentials2)
+        elif(self.name == "bishop"):
+            potentials = diagonal_potential(self, board)
+        elif(self.name == "queen"):
+            potentials1 = left_right_potential(self, board)
+            potentials2 = up_down_potential(self, board)  
+            potentials3 = diagonal_potential(self, board)
+            
+            if (potentials1 is not None):
+                potentials4 = potentials1.extend(potentials2)
+            else:
+                potentials4 = potentials2
+                
+            if (potentials4 is not None):
+                potentials = potentials4.extend(potentials3)
+            else:
+                return potentials3
+                
+        elif(self.name == "knight"):
+            potentials = knight_potential(self, board)
+        elif(self.name == "king"):
+            potentials = king_potential(self, board)
+            
+        return potentials
+        
+        
+        
+def rook_potential(the_piece, the_board):
+    brd = the_board.board_array
+    
+
+def king_potential(the_piece, the_board):
+    brd = the_board.board_array
+    
+    x = the_piece.x
+    y = the_piece.y
+    
+    changes_in_x = [-1, 0, 1]
+    changes_in_y = [-1, 0, 1]
+    
+    potential = []
+    
+    
+    for i in changes_in_x:
+        if(x+i >= 0 and x+i < 8):
+            for p in changes_in_y:
+                if(y+p >= 0 and y+p < 8):
+                    if(i != 0 and p != 0):
+                        temp_piece = brd[x+i][y+p]
+                        if(temp_piece.name == ""):
+                            potential.append(temp_piece)
+                        else:
+                            if(temp_piece.colour != the_piece.colour):
+                                potential.append(temp_piece)
+    
+    
+    return potential
+                
+    
+
+def white_pawn_potential(the_piece, the_board):
+    brd = the_board.board_array
+    
+    x = the_piece.x
+    y = the_piece.y
+    can_double = False
+    
+    if (y == 1):
+        can_double = True
+    
+    potential = []
+    
+    if (can_double == True):
+        temp_piece = brd[x][y+1]
+        if(temp_piece.name == ""):
+            potential.append(temp_piece)
+            temp_piece = brd[x][y+2]
+            if(temp_piece.name == ""):
+                potential.append(temp_piece)
+    
+    else:
+        temp_piece = brd[x][y+1]
+        if(temp_piece.name == ""):
+            potential.append(temp_piece)
+            
+    
+    if(x+1 < 8):
+        temp_piece = brd[x+1][y+1]
+        if(temp_piece.name != ""):
+            if(temp_piece.colour != the_piece.colour):
+                potential.append(temp_piece)
+    
+    if(x-1 >=0):
+        temp_piece = brd[x-1][y+1]
+        if(temp_piece.name != ""):
+            if(temp_piece.colour != the_piece.colour):
+                potential.append(temp_piece)
+                
+    return potential
+
+
+def black_pawn_potential(the_piece, the_board):
+    brd = the_board.board_array
+    
+    x = the_piece.x
+    y = the_piece.y
+    can_double = False
+    
+    if (y == 6):
+        can_double = True
+    
+    potential = []
+    
+    if (can_double == True):
+        temp_piece = brd[x][y-1]
+        if(temp_piece.name == ""):
+            potential.append(temp_piece)
+            temp_piece = brd[x][y-2]
+            if(temp_piece.name == ""):
+                potential.append(temp_piece)
+    
+    else:
+        temp_piece = brd[x][y-1]
+        if(temp_piece.name == ""):
+            potential.append(temp_piece)
+    
+    if(x+1 < 8):
+        temp_piece = brd[x+1][y-1]
+        if(temp_piece.name != ""):
+            if(temp_piece.colour != the_piece.colour):
+                potential.append(temp_piece)
+    
+    if(x-1 >=0):
+        temp_piece = brd[x-1][y-1]
+        if(temp_piece.name != ""):
+            if(temp_piece.colour != the_piece.colour):
+                potential.append(temp_piece)
+                
+    
+    return potential
+    
+
+def knight_potential(the_piece, the_board):
+    brd = the_board.board_array
+
+    change_x = [-2, -2, -1, -1, 1, 1, 2, 2]
+    change_y = [-1, 1, -2, 2, -2, 2, -1, 1]
+    
+    
+    potential = []
+    
+    x = the_piece.x
+    y = the_piece.y
+    
+    for i in range(0, len(change_x)):
+        chg_x = change_x[i]
+        chg_y = change_y[i]
+        
+        new_pos_x = chg_x+x
+        new_pos_y = chg_y+y 
+        
+        if (new_pos_x>=0 and new_pos_x<8 and new_pos_y>=0 and new_pos_y<8):
+            
+            temp_piece = brd[new_pos_x][new_pos_y]
+            
+            if (temp_piece.name == ""):
+                potential.append(temp_piece)
+            else:
+                if (temp_piece.colour != the_piece.colour):
+                    potential.append(temp_piece)
+                    
+                    
+    return potential
+        
+    
+
+
+def diagonal_potential(the_piece, brd):
+    
+    x = the_piece.x
+    y = the_piece.y
+    potential = []
+    
+    
+    
+    blocked = 1
+    count = 1
+    while(blocked == 0 and (x+count) < 8 and (y+count) < 8):
+        temp_piece = brd[x+count][y+count]
+        if (temp_piece.name == ""):
+            potential.append(temp_piece)
+        else:
+            if (temp_piece.colour != the_piece.colour):
+                potential.append(temp_piece)
+                blocked = 1
+                break;
+            else:
+                blocked = 1
+                break;
+                
+        count = count+1
+        
+        
+    blocked = 1
+    count = 1
+    while(blocked == 0 and (x+count) < 8 and (y-count) >= 0):
+        temp_piece = brd[x+count][y-count]
+        if (temp_piece.name == ""):
+            potential.append(temp_piece)
+        else:
+            if (temp_piece.colour != the_piece.colour):
+                potential.append(temp_piece)
+                blocked = 1
+                break;
+            else:
+                blocked = 1
+                break;
+                
+        count = count+1
+        
+        
+    blocked = 1
+    count = 1
+    while(blocked == 0 and (x-count) >=0 and (y+count) < 8):
+        temp_piece = brd[x-count][y+count]
+        if (temp_piece.name == ""):
+            potential.append(temp_piece)
+        else:
+            if (temp_piece.colour != the_piece.colour):
+                potential.append(temp_piece)
+                blocked = 1
+                break;
+            else:
+                blocked = 1
+                break;
+                
+        count = count+1
+        
+        
+    blocked = 1
+    count = 1
+    while(blocked == 0 and (x-count) >= 0 and (y-count) >= 0):
+        temp_piece = brd[x+count][y]
+        if (temp_piece.name == ""):
+            potential.append(temp_piece)
+        else:
+            if (temp_piece.colour != the_piece.colour):
+                potential.append(temp_piece)
+                blocked = 1
+                break;
+            else:
+                blocked = 1
+                break;
+                
+        count = count+1
+
+
+    return potential
+
+
+def left_right_potential(the_piece, brd):
+
+    
+    
+    x = the_piece.x
+    y = the_piece.y
+    potential = []
+    
+    
+    #Go Right
+    blocked = 1
+    count = 1
+    while(blocked == 0 and (x+count) < 8):
+        temp_piece = brd[x+count][y]
+        if (temp_piece.name == ""):
+            potential.append(temp_piece)
+        else:
+            if (temp_piece.colour != the_piece.colour):
+                potential.append(temp_piece)
+                blocked = 1
+                break;
+            else:
+                blocked = 1
+                break;
+                
+        count = count+1
+        
+        
+    blocked = 1
+    count = 1
+    while(blocked == 0 and (x-count) >= 0):
+        temp_piece = brd[x-count][y]
+        if (temp_piece.name == ""):
+            potential.append(temp_piece)
+        else:
+            if (temp_piece.colour != the_piece.colour):
+                potential.append(temp_piece)
+                blocked = 1
+                break;
+            else:
+                blocked = 1
+                break;
+                
+        count = count+1
+        
+    return potential
+    
+    
+def up_down_potential(the_piece, board):
+    
+    brd = board.board_array
+
+    x = the_piece.x
+    y = the_piece.y    
+
+    potential = []
+    
+    blocked = 1
+    count = 1
+    while(blocked == 0 and (y+count) < 8):
+        temp_piece = brd[x][y+count]
+        if (temp_piece.name == ""):
+            potential.append(temp_piece)
+        else:
+            if (temp_piece.colour != the_piece.colour):
+                potential.append(temp_piece)
+                blocked = 1
+                break;
+            else:
+                blocked = 1
+                break;
+                
+        count = count+1
+                
+    blocked = 0
+    count = 1      
+    while(blocked == 0 and (y-count) >=0):
+        temp_piece = brd[x][y-count]
+        if (temp_piece.name == ""):
+            potential.append(temp_piece)
+        else:
+            if (temp_piece.colour != the_piece.colour):
+                potential.append(temp_piece)
+                blocked = 1
+            else:
+                blocked = 1
+                break;
+            
+        count = count+1
+    
+    return potential
+                
+                
+
+def print_potentials(the_potentials, the_piece):
+    
+    if (the_potentials is not None):
+        if (len(the_potentials)> 0):
+            print(the_piece.name+" : ", end = "")
+            for x in the_potentials:
+                pos_string = Board.convert_spot(x)
+                print(pos_string+" ", end = "")
+        
+            print()
